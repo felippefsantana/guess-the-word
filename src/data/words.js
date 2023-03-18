a493c6c19ed06ffbad642e42e62906672553d248
Felippe Santana <felippefsantana02@gmail.com>
2023-03-17 21:59:59 -0300

    starting project

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ['motor', 'porta', 'capô', 'pneu', 'antena'],
+  fruta: ['banana', 'maçã', 'pêra', 'mamão', 'laranja'],
+  corpo: ['braço', 'perna', 'cérebro', 'pescoço', 'olhos'],
+  computador: ['mouse', 'teclado', 'montor', 'gabinete'],
+  programação: ['linguagem', 'framework', 'javascript', 'react', 'php', 'angular', 'java'],
+  alimento: ['arroz', 'feijão', 'carne', 'leite', 'ovo']
+};
