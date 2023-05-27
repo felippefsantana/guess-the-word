e8aa60748c286ba0bbec6f7bd066c3143abbe6e3
Felippe Santana <felippefsantana02@gmail.com>
2023-05-26 23:08:40 -0300

    adding functionality

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,36 +1,139 @@
-import { Link } from 'react-router-dom';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { wordsList } from '../../data/words';
+
 import styles from './styles.module.css';
 
+const guessesQty = 5;
+
 const Game = () => {
+  const navigate = useNavigate();
+  
+  const letterInputRef = useRef(null);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState('');
+  const [pickedCategory, setPickedCategory] = useState('');
+  const [letters, setLetters] = useState([]);
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(guessesQty);
+  const [score, setScore] = useState(0);
+  const [letter, setLetter] = useState('');
+
+  const pickWordAndCategory = useCallback(() => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category };
+  }, [words]);
+
+  const startGame = useCallback(() => {
+    clearLetterStates();
+
+    const { word, category } = pickWordAndCategory();
+    let wordLetters = word.split('');
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+  }, [pickWordAndCategory]);
+
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+
+    if (guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)) return;
+
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
+  }
+
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter);
+
+    setLetter('');
+    letterInputRef.current.focus();
+  }
+
+  useEffect(() => {
+    if (guesses <= 0) {
+      clearLetterStates();
+      navigate('/gameover');
+    }
+  }, [guesses, navigate]);
+
+  useEffect(() => {
+    const uniqueLetters = [...new Set(letters)];
+
+    if (uniqueLetters.length && guessedLetters.length === uniqueLetters.length) {
+      setScore((actualScore) => (actualScore += 100));
+      setGuesses(guessesQty);
+      startGame();
+    }
+  }, [guessedLetters, letters, startGame]);
+
+  useEffect(() => {
+    const initializeGame = () => {
+      startGame();
+    };
+  
+    initializeGame();
+  }, [startGame]);
+
   return (
     <div className={`${styles.game} d-flex justify-content-center align-items-center`}>
       <div className="text-center">
-        <p className={styles.points}>Pontuação: <span className="text-success"><b>0</b></span></p>
+        <p className={styles.points}>Pontuação: <span className="text-success"><b>{score}</b></span></p>
         <h1>Adivinhe a palavra:</h1>
         <h3 className={styles.tip}>
-          Dica sobre a palavra: <span><b>Dica</b></span>
+          Dica sobre a palavra: <span><b>{ pickedCategory }</b></span>
         </h3>
-        <p>Você ainda tem [X] tentativa(s).</p>
+        <p>Você ainda tem <b>{ guesses }</b> tentativa(s).</p>
 
         <div className={`${styles.wordContainer} d-flex m-3 p-3`}>
-          <span className={`${styles.blankSquare} fs-1 border rounded`}>t</span>
-          <span className={`${styles.blankSquare} fs-1 border rounded`}></span>
-          <span className={`${styles.letter} fs-1 border rounded`}></span>
-          <span className={`${styles.blankSquare} fs-1 border rounded`}>t</span>
-          <span className={`${styles.blankSquare} fs-1 border rounded`}>e</span>
+          {letters.map((letter, i) => (
+            guessedLetters.includes(letter) ? (
+              <span key={i} className={`${styles.letter} fs-1 border rounded`}>{letter}</span>
+            ) : (
+              <span key={i} className={`${styles.blankSquare} fs-1 border rounded`}></span>
+            )
+          ))}
         </div>
 
         <div className={styles.letterContainer}>
           <p>Tente adivinhar uma letra da palavra:</p>
-          <form className="d-flex justify-content-center align-items-center">
+          <form className="d-flex justify-content-center align-items-center" onSubmit={handleSubmit}>
             <input 
               type="text"
               name="letter"
               className={`${styles.letter} text-center fs-1 border rounded me-3`}
-              maxlength="1"
+              maxLength="1"
+              onChange={(e) => setLetter(e.target.value)}
+              value={letter}
+              ref={letterInputRef}
             />
-            <Link to="/gameover" className="btn btn-success rounded-pill fs-3 px-5">Jogar!</Link>
-            {/* <button className="btn btn-success rounded-pill fs-3 px-5">Jogar!</button> */}
+            <button className="btn btn-success rounded-pill fs-3 px-5">Jogar!</button>
           </form>
         </div>
 
--- src/pages/GameOver/GameOver.js
+++ src/pages/GameOver/GameOver.js
@@ -1,14 +1,22 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 import styles from './styles.module.css';
 
 const GameOver = () => {
+  const navigate = useNavigate();
+
+  const retry = () => {
+    // setScore(0);
+    // setGuesses(guessesQty);
+    navigate('/');
+  }
+
   return (
     <div className={`${styles.gameOver} d-flex justify-content-center align-items-center`}>
       <div className="text-center">
         <h1>Fim de Jogo!</h1>
         <h2>A sua pontuação foi: <span>1000</span></h2>
-        <Link to="/" className="btn btn-outline-dark rounded-pill">Tentar novamente</Link>
-        {/* <button className="btn btn-outline-dark rounded-pill">Tentar novamente</button> */}
+        <button className="btn btn-outline-dark rounded-pill" onClick={retry}>Tentar novamente</button>
       </div>
     </div>
   )
